ef11307a3c723f453cc9e5cef9c50cdc76a885d3
Nathan Corbin <ncorbin94@live.com>
2018-04-25 18:44:05 -0400

    can get data on students now

--- index.php
+++ index.php
@@ -7,39 +7,26 @@
 
 	// require the autoload file
 	require_once('vendor/autoload.php');
+
 	
 	// create an instance of the Base class
 	$f3 = Base::instance();
 
 	// define a default route
 	$f3->route('GET /', function($f3) {
-
+		include("model/apiRequests.php");
 
 		if(!isset($_SESSION['user']))
 			$f3->reroute('/login');
 
 		new UserDB();
 
-		// get the user from the session
 		$user = unserialize($_SESSION['user']);
 
-		// get data using curl with user access key
-		$url = "https://canvas.instructure.com/api/v1/courses/1310362/analytics/assignments?access_token=".$user->getAccessKey();
-
-		$ch = curl_init();
-
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-
-		$data = curl_exec($ch);
-
-		curl_close($ch);
-		
-		//$data = getCourseID($user);
+		$data = getData($user->getAccessKey());
 
 		$f3->set('user', $user);
-		$f3->set('data', json_decode($data, true));
-
+		$f3->set('data', $data);
 
 		echo Template::instance()->render('view/index.html');
 	});
---
+++ model/apiRequests.php
@@ -0,0 +1,62 @@
+<?php 
+
+	use \Curl\Curl;
+
+	function getCourseIDs($access_key)
+	{
+		$url = "https://canvas.instructure.com/api/v1/courses?access_token=".$access_key;
+
+		$curl = new Curl();
+		$curl->get($url);
+
+		$json = json_encode($curl->response);
+		$data = json_decode($json);
+
+		$courseIds = array();
+
+		foreach($data as $key => $jsons) {
+			foreach($jsons as $key => $value) {
+				if($key == "id") {
+					array_push($courseIds, $value);
+				}
+			}
+		}
+
+		return $courseIds;
+	}
+
+	function getData($access_key)
+	{
+		$courseIds = getCourseIDs($access_key);
+		$userIds = array();
+
+		foreach($courseIds as $courseId) 
+		{
+			$url = "https://canvas.instructure.com/api/v1/courses/".$courseId."/enrollments?access_token=".$access_key;
+
+			$curl = new Curl();
+			$curl->get($url);
+
+			$json = json_encode($curl->response);
+			$data = json_decode($json);
+
+			foreach($data as $key => $jsons) 
+			{
+				foreach($jsons as $key => $value) 
+				{
+					/*
+					if($key == "user")
+					{
+						array_push($userIds, $value->name);
+					}*/
+
+					if($key == "grades")
+					{
+						array_push($userIds, $value->unposted_current_score);
+					}
+				}
+			}
+		}
+
+		return $userIds;
+	}
